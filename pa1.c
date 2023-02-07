cb7428b0c4a96fcd272d4ad4590920ace6a64d69
David Alexander Sousa <dsousa@eceprog1.ecn.purdue.edu>
2023-02-06 21:55:52 -0500

    final submission, dont let this happen again

--- pa1.c
+++ pa1.c
@@ -6,29 +6,7 @@
 #include "sequence.h"
 #include "shell_array.h"
 #include "shell_list.h"
-
-static int exponent(long base, long power)
-{
-	int final = base;
-	for(int exp_idx = power; exp_idx > 1; exp_idx--)
-		final = final * base;
-	
-	if(power == 0)
-		final = 1;
-
-	return final;
-}
-
-static long logg(long base, long value) // Returns One Above The Largest Exponent Value For Mallocing The Sequence
-{
-	long exp = 1;
-	while(value >= base)
-	{
-		value /= base;
-		exp++;
-	}
-	return exp;
-}
+#include <limits.h>
 
 static void free_list(Node* head)
 {
@@ -37,17 +15,6 @@ static void free_list(Node* head)
 		free_list(head -> next);
 	}
 	free(head);
-}
-
-static int list_size(Node* head)
-{
-	int size = 0;
-	while(head != NULL)
-	{
-		size++;
-		head = head -> next;
-	}
-	return size;
 }
 
 int main(int argc, char* argv[]) {
@@ -60,20 +27,19 @@ int main(int argc, char* argv[]) {
 			char* output_file = argv[3];
 			
 			int size = 0;
-			long* seq = 0;
+			long comp = 0;
 
 			long* array_file = Array_Load_From_File(input_file, &size);
-			int num_ks = exponent(logg(2,size),2);
+			if(array_file == NULL)
+				return EXIT_FAILURE;
 
-			seq = Generate_2p3q_Seq(size, &num_ks);
-			Array_Shellsort(array_file, size, seq);
+			Array_Shellsort(array_file, size, &comp);
 
 			int num_longs = Array_Save_To_File(output_file, array_file, size); // Valgrind Seg Fault Here
 
 			free(array_file);
-			free(seq);
 
-			printf("%d\n", num_longs);
+			printf("%ld\n\n./pa1 -a %s %s\n", comp, input_file, output_file);
 		}
 		if(strcmp(argv[1],"-l") == 0) // ARRAY HAS BEEN CHOSEN
 		{
@@ -81,17 +47,15 @@ int main(int argc, char* argv[]) {
 			char* output_file = argv[3];
 
 			Node* list_file = List_Load_From_File(input_file);	
-			int size = list_size(list_file);
 			
-			long num_ks = 0;
-			Node* head = List_Shellsort(list_file, &num_ks);
+			long comp = 0;
+			Node* head = List_Shellsort(list_file, &comp);
 
 			int elems = List_Save_To_File(output_file, head);
 				
 			free_list(head);
 
-			printf("%d\n", elems);
-			
+			printf("%ld\n\n./pa1 -l %s %s\n", comp, input_file, output_file);	
 		}
 	}
 			
